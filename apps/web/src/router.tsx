dffcaaaa39c87ae33b7f0549a20afe8c47e9cbe9
Tom Gries <kontakt.tomgries@gmail.com>
2026-09-02 15:30:34 +0200

    small but fucktup fix

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,9 +1,18 @@
+import { authClient } from '@inkink/api'
 import { createInkRouter } from '@inkink/routing'
 import { inks } from './inks'
 import { Route as rootRoute } from './routes/__root'
 
 export function getRouter() {
-  return createInkRouter(rootRoute, inks)
+  return createInkRouter(rootRoute, inks, {
+    isAuthenticated: async () => {
+      const { data } = await authClient.getSession()
+
+      return Boolean(data?.user)
+    },
+
+    loginPath: '/',
+  })
 }
 
 declare module '@tanstack/react-router' {
